fb5b10e2348441f50ed3563b4ae948ba7bc92f30
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2023-10-12 12:07:28 -0300

    Recurso update

--- todolist/src/main/java/br/com/danieleleao/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/danieleleao/todolist/filter/FilterTaskAuth.java
@@ -25,35 +25,31 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-
-		
-		String authorization = request.getHeader("Authorization");
-		String authEncoded = authorization.substring("Basic".length()).trim();
-		byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-		String authString = new String(authDecode);
-		String[] credentials = authString.split(":");
-		String username = credentials[0];
-		String password = credentials[1];
-		
-
-		UserModel user = this.userRepository.findByUsername(username);
-		if(user == null) {
-			response.sendError(401);
-		} else {
-			
-			Result passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-			if(passwordVerify.verified) {
-				filterChain.doFilter(request, response);
-			} else {
+		System.out.println("Chegou no filtro");
+		String servletPath = request.getServletPath();
+		if(servletPath.startsWith("/tasks/")) {
+			String authorization = request.getHeader("Authorization");
+			String authEncoded = authorization.substring("Basic".length()).trim();
+			byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+			String authString = new String(authDecode);
+			String[] credentials = authString.split(":");
+			String username = credentials[0];
+			String password = credentials[1];
+			UserModel user = this.userRepository.findByUsername(username);
+			if(user == null) {
 				response.sendError(401);
+			} else {
+				Result passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+				if(passwordVerify.verified) {
+					request.setAttribute("idUser", user.getId());
+					filterChain.doFilter(request, response);
+				} else {
+					response.sendError(401);
+				}
 			}
-			
-			
+		} else {
+			filterChain.doFilter(request, response);
 		}
-		
 	}
-
-
-
 
 }
